267e0481ee747e0c4275a77db2a6ffd0136cae7b
alpha-tango-kilo <git@heyatk.com>
2023-04-22 17:21:17 +0100

    Discover subtitles
    Fix discovery not descending into folders

--- src/main.rs
+++ src/main.rs
@@ -27,15 +27,11 @@ fn main() {
 fn process(path: impl AsRef<Utf8Path>) -> anyhow::Result<()> {
     info!("discovering video files in {}", path.as_ref());
     let videos = discover_videos(path.as_ref());
-    info!("videos in {}: {videos:?}", path.as_ref());
     match videos.len() {
-        0 => {
-            bail!("didn't find any videos in {}", path.as_ref());
-        },
-        1 => {
-            todo!()
-        },
+        0 => bail!("didn't find any videos in {}", path.as_ref()),
+        1 => info!("found {}", &videos[0]),
         _ => {
+            info!("videos in {}: {videos:?}", path.as_ref());
             if !predicates::different_versions_same_media(videos.iter()) {
                 bail!(
                     "unsure that all videos are different versions of the \
@@ -47,6 +43,12 @@ fn process(path: impl AsRef<Utf8Path>) -> anyhow::Result<()> {
             );
         },
     }
+    let subs = discover_subtitles(path.as_ref());
+    if subs.is_empty() {
+        info!("no subtitles found in {}, nothing to do", path.as_ref());
+        return Ok(());
+    }
+    info!("subtitles in {}: {subs:?}", path.as_ref());
     Ok(())
 }
 
@@ -56,25 +58,61 @@ fn discover_videos(in_dir: impl AsRef<Utf8Path>) -> Vec<Utf8PathBuf> {
         .max_depth(1)
         .contents_first(true)
         .into_iter()
-        .filter_entry(predicates::is_video)
         .filter_map(|dir_entry| match dir_entry {
-            Ok(ent) => Some(ent.path().to_owned()),
+            Ok(dir_entry) => Some(dir_entry),
             Err(why) => {
                 warn!("{why}");
                 None
             },
         })
-        .filter_map(|path| match Utf8PathBuf::try_from(path.clone()) {
-            Ok(path) => Some(path),
-            Err(_) => {
-                warn!("skipped non-UTF-8 path {}", path.display());
+        .filter(predicates::is_video)
+        .filter_map(|dir_entry| {
+            match Utf8PathBuf::try_from(dir_entry.path().to_owned()) {
+                Ok(path) => Some(path),
+                Err(_) => {
+                    warn!(
+                        "skipped non-UTF-8 path {}",
+                        dir_entry.path().display()
+                    );
+                    None
+                },
+            }
+        })
+        .collect()
+}
+
+fn discover_subtitles(in_root_dir: impl AsRef<Utf8Path>) -> Vec<Utf8PathBuf> {
+    WalkDir::new(in_root_dir.as_ref())
+        .min_depth(1)
+        .sort_by_file_name()
+        .follow_links(false)
+        .into_iter()
+        .filter_map(|dir_entry| match dir_entry {
+            Ok(dir_entry) => Some(dir_entry),
+            Err(why) => {
+                warn!("{why}");
                 None
             },
+        })
+        .filter(predicates::is_subtitle)
+        .filter_map(|dir_entry| {
+            match Utf8PathBuf::try_from(dir_entry.path().to_owned()) {
+                Ok(path) => Some(path),
+                Err(_) => {
+                    warn!(
+                        "skipped non-UTF-8 path {}",
+                        dir_entry.path().display()
+                    );
+                    None
+                },
+            }
         })
         .collect()
 }
 
 mod predicates {
+    use std::ffi::OsStr;
+
     use camino::Utf8PathBuf;
     use log::{error, info, trace};
     use once_cell::sync::Lazy;
@@ -82,6 +120,7 @@ mod predicates {
     use walkdir::DirEntry;
 
     const VIDEO_EXTENSIONS: &[&str] = &["mp4", "mkv"];
+    const SUBTITLE_EXTENSIONS: &[&str] = &["srt", "vtt", "idx"];
 
     static QUALITY_SUFFIX_REGEX: Lazy<Regex> = Lazy::new(|| {
         RegexBuilder::new(r#" - ((720p)|(1080p)|(4K( HDR)?))$"#)
@@ -90,15 +129,33 @@ mod predicates {
             .unwrap()
     });
 
+    fn ext_in(ext: &OsStr, group: &[&str]) -> bool {
+        group
+            .iter()
+            .any(|acceptable| ext.eq_ignore_ascii_case(acceptable))
+    }
+
     pub fn is_video(dir_entry: &DirEntry) -> bool {
         dir_entry.file_type().is_file()
             && dir_entry
                 .path()
                 .extension()
                 .map(|ext| {
-                    VIDEO_EXTENSIONS
-                        .iter()
-                        .any(|vid_ext| ext.eq_ignore_ascii_case(vid_ext))
+                    trace!("seeing if {ext:?} is a video extension");
+                    ext_in(ext, VIDEO_EXTENSIONS)
+                })
+                .unwrap_or_default()
+    }
+
+    pub fn is_subtitle(dir_entry: &DirEntry) -> bool {
+        trace!("testing {dir_entry:?}");
+        dir_entry.file_type().is_file()
+            && dir_entry
+                .path()
+                .extension()
+                .map(|ext| {
+                    trace!("seeing if {ext:?} is a subtitle extension");
+                    ext_in(ext, SUBTITLE_EXTENSIONS)
                 })
                 .unwrap_or_default()
     }
